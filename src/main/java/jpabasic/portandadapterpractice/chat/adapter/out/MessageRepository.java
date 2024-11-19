983132add056d5eaf99385d79ecbecb97792377d
coen <coen@mrblue.com>
2024-11-19 13:08:30 +0900

    fix: remove wrong commit class file

--- src/main/java/jpabasic/portandadapterpractice/chat/adapter/out/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package jpabasic.portandadapterpractice.chat.adapter.out;
-
-import jpabasic.portandadapterpractice.chat.adapter.out.entity.MessageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
-}
